344e06ade8d852d2cc2b0f2d00394f914b3df921
hw1 <lilia_dali@mail.ru>
2023-06-24 01:36:50 +0700

    Добавила компонент MainNav

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainNav from './components/mainNav';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('.wrapper'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainNav />
   </React.StrictMode>
 );
 
